51e3ae65280b943b4ecfa967165fe149fd200c8f
Facundo <facundosu26@gmail.com>
2025-10-18 03:08:08 -0300

    formulario hecho

--- Front end/src/componentes/producto/FormularioDinamico.jsx
+++ Front end/src/componentes/producto/FormularioDinamico.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Box, TextField, Button, Paper, Typography } from "@mui/material";
+import Sidebar from "../navbar/Sidebar";
 
 export default function DynamicForm({ fields = [], onSubmit, title }) {
   const [formData, setFormData] = useState(
@@ -27,6 +28,7 @@ export default function DynamicForm({ fields = [], onSubmit, title }) {
         p: 2,
       }}
     >
+      <Sidebar />
       <Paper
         elevation={3}
         sx={{ p: 4, borderRadius: 2, width: "100%", maxWidth: 400 }}
